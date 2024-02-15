85198351737009e7b7c2682521e34813df5f479e
Muhamad Nahrowi <86625224+m-nahrowi@users.noreply.github.com>
2024-02-15 14:54:40 +0700

    add "api http post"

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:persiapan_flutix/post_result_model.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Null postResult = null;
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primaryColor: Colors.blue,),
-      home: 
+      home: Scaffold(
+        appBar: AppBar(title: Text("API Demo")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text((postResult != null)
+                  ? postResult.id +
+                      " | " +
+                      postResult.name +
+                      " | " +
+                      postResult.job +
+                      " | " +
+                      postResult.created
+                  : ' Tidak ada data '),
+              ElevatedButton(onPressed: () {}, child: Text("POST")),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
